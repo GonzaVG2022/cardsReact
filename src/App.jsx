f3e1cad99bc7c14b2d6cc36e05c5d72ebb75906c
Flavio Gonzalo Villalba Gargantini <gonzagargantini20@gmail.com>
2022-12-21 18:11:57 -0300

    inicio-proyecto

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <h2>holis</h2>
+      <i class='bx bxs-cat'></i>
+    </div>
+  )
+}
+
+export default App
