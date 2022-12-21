0e94918300851387f79724a2cc48a40ebd40826f
Flavio Gonzalo Villalba Gargantini <gonzagargantini20@gmail.com>
2022-12-21 20:57:12 -0300

    pausa

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 import { useState } from 'react'
+import users from './json/users'
+import Users from './assets/components/structur'
+
+
 
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
-
+const userArray = users.map((name, index) => <Users key={index} count={name}/>)
   return (
     <div className="App">
-      <h2>holis</h2>
-      <i class='bx bxs-cat'></i>
+      
+      
+     
     </div>
   )
 }
---
+++ src/assets/components/structur.jsx
@@ -0,0 +1,10 @@
+
+const Users = ({users})=> {
+    const [gender, name, location,email,login, dob,registered,phone,picture,nat]=users
+
+    return(
+<h1>`${gender}`</h1>
+    )
+}
+
+export default Users
